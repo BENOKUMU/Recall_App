06c446b074a355fdd14646f480da21061865d48a
Benjamin Okumu <benjaminokumu12@gmail.com>
2024-10-01 18:13:03 +0300

    update

---
+++ server/controllers/posts.js
@@ -0,0 +1,15 @@
+import postMessage from "../models/postMessage";
+
+
+export const getPosts = (req, res) => {
+    try{
+
+    } catch (error) {
+
+    }
+    // res.send('It\'s Okay 👌')
+};
+
+export const createPost = (req, res) => {
+    res.send('post successful 💕');
+};
